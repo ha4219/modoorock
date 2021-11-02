df19d4625d225f52ef130c5585a844dfe725f531
정동하 <ha4219@naver.com>
2021-11-03 03:18:11 +0900

    [ha4219] add button onPress Linking

--- src/components/HomeAdvertise.js
+++ src/components/HomeAdvertise.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useCallback} from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Alert,
+  Linking,
+} from 'react-native';
 
 import Config from 'react-native-config';
 
 const HomeAdvertise = ({item}) => {
+
+  const handlePress = useCallback(async () => {
+    // Checking if the link is supported for links with custom URL scheme.
+    const supported = await Linking.canOpenURL(item.item.content);
+
+    if (supported) {
+      // Opening the link with some app, if the URL scheme is "http" the web link should be opened
+      // by some browser in the mobile
+      await Linking.openURL(item.item.content);
+    } else {
+      Alert.alert(`Don't know how to open this URL: ${item.item.content}`);
+    }
+  }, [item.item.content]);
   return (
-    <TouchableOpacity styles={styles.item} activeOpacity={0.7}>
+    <TouchableOpacity
+      styles={styles.item}
+      activeOpacity={0.7}
+      onPress={() => handlePress()}>
       <Image
         style={styles.image}
         source={require('../assets/tempAdvertise.jpeg')}
